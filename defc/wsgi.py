b9afab0542d640844fd0d39db74858fb090c3fec
DEFC <defc@hephaistos>
2015-08-05 16:43:38 +0200

    Initial commit

---
+++ defc/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for defc project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "defc.settings"
+
+application = get_wsgi_application()
