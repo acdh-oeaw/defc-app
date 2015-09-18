b080dc401da3a0eba16c9f150a77646303c7dc71
zxenia <ksenia1zaytseva@gmail.com>
2015-09-18 12:14:52 +0200

    changes in server.py changes in create_area template

--- orea/settings/server.py
+++ orea/settings/server.py
@@ -24,7 +24,7 @@ DATABASES = {
         'NAME': 'defc',
         'USER': 'defc',
         'PASSWORD': 'F7ffq5WUVp',
-        'HOST': 'localhost',
+        'HOST': 'helios.arz.oeaw.ac.at',
         'PORT': '3306',
     }
 }
