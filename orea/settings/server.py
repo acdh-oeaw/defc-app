dc53bffbc026320d63a58100d480199c818302e0
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2015-10-19 16:29:17 +0200

    first attempt to create resizable multiple selction fields using jquery UI

--- orea/settings/server.py
+++ orea/settings/server.py
@@ -24,7 +24,8 @@ DATABASES = {
         'NAME': 'defc',
         'USER': 'defc',
         'PASSWORD': 'F7ffq5WUVp',
-        'HOST': 'helios.arz.oeaw.ac.at',
+        #'HOST': 'helios.arz.oeaw.ac.at',
+		'HOST': 'localhost',
         'PORT': '3306',
     }
 }
