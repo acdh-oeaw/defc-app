da14e5c7493f8404cec7a23c0512d35ba72a0aa3
zxenia <ksenia1zaytseva@gmail.com>
2016-03-24 14:49:37 +0100

    disabled some fields from editing in area and finds fixed jquery for edit_area and edit_finds fixed editing forms as we planned

--- defcdb/forms.py
+++ defcdb/forms.py
@@ -47,6 +47,26 @@ class AreaForm(autocomplete_light.ModelForm):
 			super(AreaForm, self).__init__(*args, **kwargs)
 			self.helper = FormHelper()
 			self.helper.add_input(Submit('submit','Create'))
+			instance = getattr(self, 'instance', None)
+			if instance and instance.pk:
+				self.fields['site'].widget.attrs['readonly'] = True    #lock some fields in editing mode from editing
+				self.fields['site'].widget.attrs['disabled'] = True
+				self.fields['area_type'].widget.attrs['readonly'] = True
+				self.fields['area_type'].widget.attrs['disabled'] = True
+
+	def clean_site(self):
+		instance = getattr(self, 'instance', None)
+		if instance and instance.pk:
+			return instance.site
+		else:
+			return self.cleaned_data['site']
+
+	def clean_area_type(self):
+		instance = getattr(self, 'instance', None)
+		if instance and instance.pk:
+			return instance.area_type
+		else:
+			return self.cleaned_data['area_type']
 
 
 class ResearcheventForm(autocomplete_light.ModelForm):
@@ -83,11 +103,38 @@ class FindsForm(autocomplete_light.ModelForm, forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 			super(FindsForm, self).__init__(*args, **kwargs)
 			self.helper = FormHelper()
-			self.helper.add_input(Submit('submit','Create'))  #http://stackoverflow.com/questions/21559380/django-crispy-forms-readonly
-			# self.helper.layout = Layout('email', Field('area', disabled =True),
-   #  )
+			self.helper.add_input(Submit('submit','Create'))
+			instance = getattr(self, 'instance', None)
+			if instance and instance.pk:
+				self.fields['area'].widget.attrs['readonly'] = True
+				self.fields['area'].widget.attrs['disabled'] = True
+				self.fields['finds_type'].widget.attrs['readonly'] = True
+				self.fields['finds_type'].widget.attrs['disabled'] = True
+
+	def clean_area(self):
+		instance = getattr(self, 'instance', None)
+		if instance and instance.pk:
+			return instance.area
+		else:
+			return self.cleaned_data['area']
+
+	def clean_finds_type(self):
+		instance = getattr(self, 'instance', None)
+		if instance and instance.pk:
+			return instance.finds_type
+		else:
+			return self.cleaned_data['finds_type']
 
 
+	# def clean_finds_type(self):  ###second option, also works
+	# 	if self.instance:
+	# 		return self.instance.area
+	# 	else:
+	# 		return self.cleaned_data.get('area')
+
+	#http://stackoverflow.com/questions/324477/in-a-django-form-how-to-make-a-field-readonly-or-disabled-so-that-it-cannot-b
+
+	
 class SiteForm(autocomplete_light.ModelForm):
 	reference = autocomplete_light.ModelMultipleChoiceField(Book.objects.all(),
 		required = False,
