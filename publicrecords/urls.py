7f5a6011b6bef1f3b1feca6126cc56d3cdeca093
zxenia <ksenia1zaytseva@gmail.com>
2016-03-29 16:23:54 +0200

    created publicrecords app

---
+++ publicrecords/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from . import views
+from defcdb.urls import *
+
+# urlpatterns = [
+
+# ]
---
+++ publicrecords/views.py
@@ -0,0 +1,167 @@
+from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.template import RequestContext
+from django.views import generic
+from django.views.generic.detail import DetailView
+
+from defcdb.models import Site, Area, ResearchEvent, Finds, Interpretation
+
+from defcdb.forms import AreaForm, ResearcheventForm, FindsForm, SiteForm, InterpretationForm
+from bib.models import Book
+
+
+#################################################################
+#				views for ResearchEvent							#
+#################################################################
+class ResearchEventListView(generic.ListView):
+	model = ResearchEvent
+	template_name = 'defcdb/researchevent_list.html'        
+	context_object_name = 'object_list' # use object_list instead of
+	# e.g. site_list so the template does not need to be changed so much
+	#for each class. 
+
+	def get_queryset(self):
+		return ResearchEvent.objects.filter(public=True).order_by('id')
+
+
+class ResearchEventDetail(DetailView):
+	model = ResearchEvent
+	def get_context_data(self, **kwargs):
+		context = super(ResearchEventDetail, self).get_context_data(**kwargs)
+		current_object = self.object
+		context['researchtype_list'] = current_object.research_type.all()
+		context['institution_list'] = current_object.institution.all()
+		context['specialanalysis_list'] = current_object.special_analysis.all()
+		context['reference_list'] = current_object.reference.all()
+		return context
+
+
+#################################################################
+#				views for Finds									#
+#################################################################
+class FindsListView(generic.ListView):
+	template_name = 'defcdb/finds_list.html' 
+	context_object_name = 'object_list' # use object_list instead of
+	# e.g. site_list so the template does not need to be changed so much
+	#for each class. 
+
+	def get_queryset(self):
+		return Finds.objects.filter(public=True).order_by('finds_type')
+
+
+class FindsDetail(DetailView):
+	model = Finds
+	def get_context_data(self, **kwargs):
+		context = super(FindsDetail, self).get_context_data(**kwargs)
+		current_find = self.object
+		context['area_list'] = Area.objects.filter(finds=current_find.id)
+		context['researchevent_list'] = ResearchEvent.objects.filter(finds=current_find.id)
+		current_find_2 = self.object
+		context['interpretation_list'] = Interpretation.objects.filter(finds=current_find_2.id)
+		context['smallfindstype_list'] = current_find.small_finds_type.all()
+		context['botanyspecies_list'] = current_find.botany_species.all()
+		context['animalremainsspecies_list'] = current_find.animal_remains_species.all()
+		context['animalremainspart_list'] = current_find.animal_remains_part.all()
+		context['lithicstechnology_list'] = current_find.lithics_technology.all()
+		context['lithicsindustry_list'] = current_find.lithics_industry.all()
+		context['lithicscoreshape_list'] = current_find.lithics_core_shape.all()
+		context['lithicsretouchedtools_list'] = current_find.lithics_retouched_tools.all()
+		context['lithicsrawmaterial_list'] = current_find.lithics_raw_material.all()
+		context['potterydetail_list'] = current_find.pottery_detail.all()
+		context['potterydecoration_list'] = current_find.pottery_decoration.all()
+		context['material_list'] = current_find.material.all()
+		context['reference_list'] = current_find.reference.all()
+		return context
+
+
+#################################################################
+#				views for Site									#
+#################################################################
+
+class SiteListView(generic.ListView):
+	template_name = 'defcdb/site_list.html'
+
+	def get_queryset(self):
+		return Site.objects.filter(public=True).order_by('name')
+
+
+class SiteDetail(DetailView):
+	model = Site
+	def get_context_data(self, **kwargs):
+		context = super(SiteDetail, self).get_context_data(**kwargs)
+		current_site = self.object
+		context['areas_list'] = Area.objects.filter(site = current_site.id)
+		context['reference_list'] = current_site.reference.all()
+		context['aliasName_list'] = current_site.alias_name.all()
+		context['alternativeName_list'] = current_site.alternative_name.all()
+		return context
+
+		
+#################################################################
+#				views for Area									#
+#################################################################
+class AreaListView(generic.ListView):
+	template_name = 'defcdb/area_list.html'     
+	context_object_name = 'object_list' # use object_list instead of
+	# e.g. site_list so the template does not need to be changed so much
+	#for each class. 
+
+	def get_queryset(self):
+		return Area.objects.filter(public=True).order_by('area_type')
+
+
+class AreaDetail(DetailView):
+	model = Area
+	def get_context_data(self, **kwargs):
+		context = super(AreaDetail, self).get_context_data(**kwargs)
+		current_area = self.object
+		context['sites_list'] = Site.objects.filter(area=current_area.id)
+		context['period_reference_list'] = current_area.period_reference.all()
+		context['period_list'] = current_area.period.all()
+		context['dating_method_list'] = current_area.dating_method.all()
+		context['datedby_list'] = current_area.earliest_datedated_by.all()
+		context['datedby_list'] = current_area.latest_datedated_by.all()
+		context['interpretations_list'] = Interpretation.objects.filter(area=current_area.id)
+		context['finds_list'] = Finds.objects.filter(area=current_area.id)
+		context['reference_list'] = current_area.reference.all()
+		# context['settlementtype_list'] = current_area.settlement_type.all()
+		context['settlementstructure_list'] = current_area.settlement_structure.all()
+		context['settlementconstructiontype_list'] = current_area.settlement_construction_type.all()
+		context['settlementbuildingtechnique_list'] = current_area.settlement_building_technique.all()
+		context['settlementspecialfeatures_list'] = current_area.settlement_special_features.all()
+		context['evidenceofoccupation_list'] = current_area.cave_rockshelters_evidence_of_occupation.all()
+		# context['quarryexploitationtype_list'] = current_area.quarry_exploitation_type.all()
+		context['quarryrawmaterial_list'] = current_area.quarry_raw_material.all()
+		context['topography_list'] = current_area.cemetery_or_graves_topography.all()
+		context['mortuaryfeatures_list'] = current_area.cemetery_or_graves_mortuary_features.all()
+		context['gravetype_list'] = current_area.grave_type.all()
+		context['typeofhumanremains_list'] = current_area.grave_type_of_human_remains.all()
+		context['graveagegroups_list'] = current_area.grave_age_groups.all()
+		context['gravesexes_list'] = current_area.grave_sexes.all()
+		context['manipulationsofgraves_list'] = current_area.grave_manipulations_of_graves.all()
+		return context
+
+
+#################################################################
+#				views for Interpretation						#   #to be done
+#################################################################
+class InterpretationListView(generic.ListView):
+	template_name = 'defcdb/interpretation_list.html'       
+	context_object_name = 'object_list'
+
+	def get_queryset(self):
+		#return Interpretation.objects.order_by('finds')
+		return Interpretation.objects.filter(public=True).order_by('id')
+
+
+class InterpretationDetail(DetailView):
+	model = Interpretation
+	def get_context_data(self, **kwargs):
+		context = super(InterpretationDetail, self).get_context_data(**kwargs)
+		current_object = self.object
+		context['areas_list'] = current_object.area.all()
+		context['finds_list'] = current_object.finds.all()
+		context['productiontype_list'] = current_object.production_type.all()
+		context['subsistencetype_list'] = current_object.subsistence_type.all()
+		context['reference_list'] = current_object.reference.all()
+		return context
