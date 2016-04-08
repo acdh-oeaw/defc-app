6b6d282d795dabb0727ebe577c521b0fce849cbb
zxenia <ksenia1zaytseva@gmail.com>
2016-04-08 17:25:13 +0200

    added detail view for images gallery (not linked yet) created public image gallery view (no url)

--- images_metadata/urls.py
+++ images_metadata/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.ImageThesaurusListView.as_view(), name='object_list'),
     url(r'^upload/$', views.upload_file, name='upload_file'),
     url(r'^update/(?P<pk>[0-9]+)$', views.update_file, name='update_file'),
+    url(r'^detail/(?P<pk>[0-9]+)$', views.ImageThesaurusDetail.as_view(), name='detail'),
+    # url(r'^public_gallery/(?P<pk>[0-9]+)$', views.ImageThesaurusDetail.as_view(), name='detail'),
 ]
--- images_metadata/views.py
+++ images_metadata/views.py
@@ -2,22 +2,39 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.views import generic
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.core.urlresolvers import reverse, reverse_lazy
 from .models import ImageThesaurus
+from defcdb.models import DC_region
 from .forms import ImageThesaurusForm
 
 
 class ImageThesaurusListView(generic.ListView):
     model = ImageThesaurus
     template_name = 'images_metadata/image_gallery.html'
+    # template_name = 'images_metadata/public_image_gallery.html'
     context_object_name = 'object_list'
 
     def get_queryset(self):
         return ImageThesaurus.objects.all()
+
+class ImageThesaurusDetail(DetailView):
+    model = ImageThesaurus
+    template_name = 'images_metadata/image_detail.html'
+    def get_context_data(self, **kwargs):
+        context = super(ImageThesaurusDetail, self).get_context_data(**kwargs)
+        current_object = self.object
+        #context['region_list'] = current_object.region.all()
+        context['region_list'] = DC_region.objects.filter(imagethesaurus=current_object.id)
+        # context['institution_list'] = current_object.institution.all()
+        # context['specialanalysis_list'] = current_object.special_analysis.all()
+        # context['reference_list'] = current_object.reference.all()
+        # context['finds_list'] = Finds.objects.filter(research_event=current_object.id)
+        return context
 
 
 @login_required
